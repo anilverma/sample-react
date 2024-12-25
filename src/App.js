86c54aad7e2807318620d08faa8e307b415ae8ce
anil verma <leo.anil@gmail.com>
2024-12-25 22:42:50 +0530

    form

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
+import Stylesheet from "./components/Stylesheet";
+import './appStyle.css';
+import styles from './appStyles.module.css';
+import Form from "./components/Form";
+
 function App() {
   return (
-    <div className="App">
-        <Greet name="Anil">
-            <h3>This is children props.name</h3>
-        </Greet>
-        <Welcome name="Anil"></Welcome>
-        <Message />
-        <FunctionClick/>
-        <ClassClick/>
-        <EventBind/>
-        <ParentComponent/>
-    </div>
-    
+      <div className="App">
+         {/* <h1 className='error'>Error</h1>
+          <h1 className={styles.success}>Sucess</h1>
+          <Greet name="Anil">
+              <h3>This is children props.name</h3>
+          </Greet>
+          <Welcome name="Anil"></Welcome>
+          <Message/>
+          <FunctionClick/>
+          <ClassClick/>
+          <EventBind/>
+          <ParentComponent/>
+          <Stylesheet primary={true}/>*/}
+            <Form/>
+      </div>
+
   );
 }
 
